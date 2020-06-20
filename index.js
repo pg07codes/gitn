e9af6d096897e35e8a3e4ba73471fb7348b360db
pg07codes <pg07codes@gmail.com>
2020-06-20 21:35:43 +0530

    added commit,branch,checkout vers:1

--- index.js
+++ index.js
@@ -1,40 +1,103 @@
 const { program } = require('commander');
-const fs=require("fs").promises
+const fs = require("fs").promises
 program.version('0.0.1');
 
+
+// ---------config store code
+const Configstore = require('configstore');
+const cfg = new Configstore("gitn", {init: false});
+let NOTE_DIR=cfg.get('NOTE_DIR');
+let CURRENT_BRANCH=cfg.get('CURRENT_BRANCH');
+
+ 
+// ----config store code
 program
-    .option('-d, --debug', 'output extra debugging')
-    .option('-s, --small', 'small pizza size')
-    .option('-p, --pizza-type <type>', 'flavour of pizza')
-    .option('-c, --cheese <type>', 'add the specified type of cheese');
+  .command('status ')
+  .description('outputs current branch')
+  .action(() => {
+    console.log(`you are on ${CURRENT_BRANCH}`);
+  })
 
 program
   .command('log ')
-  .description('logs your notes')
-  .action(async () =>{
+  .description('outputs notes on current branch')
+  .action(async () => {
     let filehandle;
     try {
-      filehandle = await fs.open('/home/pg07codes/notes/notes1.txt', 'r');
-      data=await filehandle.readFile('utf-8');
+      filehandle = await fs.open(`${NOTE_DIR}${CURRENT_BRANCH}.txt`, 'r');
+      data = await filehandle.readFile('utf-8');
       console.log(data)
     } finally {
       if (filehandle !== undefined)
         await filehandle.close();
     }
   })
-    
-
-      
-        
 
 program
-  .command('start <service>', 'start named service')
-  .command('stop [service]', 'stop named service, or all if no name supplied');
+  .command('commit')
+  .requiredOption('-m , --message <msg>', 'commit must have a mesage')
+  .action(async (args) => {
+    let filehandle;
+    try {
+      filehandle = await fs.open(`${NOTE_DIR}${CURRENT_BRANCH}.txt`, 'a', 666);
+      await fs.appendFile(filehandle, args.message + '\n', 'utf-8');
+    } finally {
+      if (filehandle !== undefined)
+        await filehandle.close();
+    }
+
+  })
+
+
+program
+  .command('branch [newBranch]')
+  .action(async (newBranch) => {
+    let filehandle;
+    if (!newBranch) {
+      try {
+        filehandle = await fs.readdir(`${NOTE_DIR}`, 'utf-8');
+        filehandle.forEach(e => {
+          console.log(e);
+        })
+      } catch (e) {
+        console.log('some error occured:', e)
+      }
+    } else {
+      try {
+        filehandle = await fs.open(`${NOTE_DIR}${newBranch}.txt`,'wx'); // wx fails if exist, so may use w 
+        // console.log(filehandle)
+      } finally {
+        if (filehandle !== undefined)
+          await filehandle.close();
+      }
+    }
+
+  })
+
+
+program
+  .command('checkout <branch>')
+  .description('switch to different branch')
+  .action( (branch) => {
+    cfg.set('CURRENT_BRANCH',`${branch}`)
+  })
+
+
+// program
+//   .command('checkout')
+//   .action(async (arg) => {
+//     let filehandle;
+//     try {
+//       filehandle = await fs.readdir(`${NOTE_DIR}`, 'utf-8');
+//       filehandle.forEach(e => {
+//         console.log(e);
+//       })
+//     } catch (e) {
+//       console.log('some error occured:', e)
+//     }
+
+//   })
+
 
 
 program.parse(process.argv);
-
-if (program.debug) console.log(program.opts());
-if (program.small) console.log('- small pizza size');
-if (program.pizzaType) console.log(`- ${program.pizzaType}`);
-if (program.cheese) console.log(`cheese: ${program.cheese}`);
