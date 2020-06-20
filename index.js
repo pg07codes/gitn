431310f6e239ec002742a06e90c0afd2a48b6c44
pg07codes <pg07codes@gmail.com>
2020-06-20 23:25:29 +0530

    merge added, some err catched and handles

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ program.version('0.0.1');
 
 // ---------config store code
 const Configstore = require('configstore');
-const cfg = new Configstore("gitn", {init: false});
-let NOTE_DIR=cfg.get('NOTE_DIR');
-let CURRENT_BRANCH=cfg.get('CURRENT_BRANCH');
+const cfg = new Configstore("gitn", { init: false });
+let NOTE_DIR = cfg.get('NOTE_DIR');
+let CURRENT_BRANCH = cfg.get('CURRENT_BRANCH');
 
- 
+
 // ----config store code
 program
   .command('status ')
@@ -57,6 +57,7 @@ program
       try {
         filehandle = await fs.readdir(`${NOTE_DIR}`, 'utf-8');
         filehandle.forEach(e => {
+          e = e.split('.')[0] // only file(branch) name
           console.log(e);
         })
       } catch (e) {
@@ -64,8 +65,14 @@ program
       }
     } else {
       try {
-        filehandle = await fs.open(`${NOTE_DIR}${newBranch}.txt`,'wx'); // wx fails if exist, so may use w 
+        filehandle = await fs.open(`${NOTE_DIR}${newBranch}.txt`, 'wx'); // wx fails if exist, so may use w 
         // console.log(filehandle)
+      } catch (e) {
+        if (e.code == 'EEXIST') {
+          console.log('branch already exists')
+        } else {
+          console.log(e)
+        }
       } finally {
         if (filehandle !== undefined)
           await filehandle.close();
@@ -78,25 +85,38 @@ program
 program
   .command('checkout <branch>')
   .description('switch to different branch')
-  .action( (branch) => {
-    cfg.set('CURRENT_BRANCH',`${branch}`)
+  .action((branch) => {
+    cfg.set('CURRENT_BRANCH', `${branch}`)
   })
 
+program
+  .command('merge <branch>')
+  .description('merge branch into current branch')
+  .action(async (branch) => {
+    let filehandle;
+    let currentFilehandle;
+    try {
+      if (branch === CURRENT_BRANCH) throw ({ errno: 8989, msg: 'on same branch' })
+      filehandle = await fs.open(`${NOTE_DIR}${branch}.txt`, 'r');
+      data = await filehandle.readFile('utf-8');
+      currentFilehandle = await fs.open(`${NOTE_DIR}${CURRENT_BRANCH}.txt`, 'a', 666);
+      await fs.appendFile(currentFilehandle, data, 'utf-8');
+      await fs.unlink(`${NOTE_DIR}${branch}.txt`);
+    } catch (e) {
+      if (e.errno === -2) {
+        console.log('no such branch');
+      } else if (e.errno === 8989) {
+        console.log(e.msg);
+      } else
+        console.log(e);
+    } finally {
+      if (filehandle !== undefined)
+        await filehandle.close();
+      if (currentFilehandle !== undefined)
+        await currentFilehandle.close();
+    }
 
-// program
-//   .command('checkout')
-//   .action(async (arg) => {
-//     let filehandle;
-//     try {
-//       filehandle = await fs.readdir(`${NOTE_DIR}`, 'utf-8');
-//       filehandle.forEach(e => {
-//         console.log(e);
-//       })
-//     } catch (e) {
-//       console.log('some error occured:', e)
-//     }
-
-//   })
+  })
 
 
 
